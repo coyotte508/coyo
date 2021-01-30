c1625bc6d47f109639bc6d369a888886e8f5853a
coyotte508 <coyotte508@gmail.com>
2021-01-30 01:03:13 +0100

    Update bio

--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,44 @@
     <v-row justify="center" align-content="center" style="min-height: 80vh">
       <v-col cols="12" md="8" lg="6">
         <v-card>
-          <v-card-text>
-            <img style="float: left; height: 80px" src="/icons/logo.png" class="mr-4 mb-4">
+          <v-card-text class="pb-2">
+            <img
+              style="float: left; height: 80px"
+              src="/icons/logo.png"
+              class="mr-4 mb-4"
+            />
             <div>
               <p>
-                I have a lot of experience designing complex projects from end to end, and handling every separate aspect: server configuration, database, deployment, frontend. Nowadays I specialize towards SaaS web software with Node.js, but I have ample C++ / Qt experience as well.
+                I have a lot of experience designing complex projects from end
+                to end, and handling every separate aspect: server
+                configuration, database, deployment, frontend. Nowadays I
+                specialize towards SaaS web software with <b>Node.js</b>, but I
+                have ample <b>C++</b> / <b>Qt</b> experience as well.
               </p>
-              <p class="mb-0">
-                I like well designed code, up-to-date framework, new technologies and clean git repos. I can help you update your codebase to current standards, design a project from the ground up, or give you a helping hand in a bind.
+              <p>
+                I like well designed code, up-to-date framework, new
+                technologies and clean git repos.
+              </p>
+              <p class="mb-0 pb-0">
+                I am currently interested in <b>Rust</b> and <b>Svelte</b>
               </p>
             </div>
             <div style="clear: both" />
           </v-card-text>
           <v-card-actions>
             <v-btn href="mailto:contact@coyo.dev" target="blank">
-              <v-icon class="mr-2">
-                mdi-email
-              </v-icon>
+              <v-icon class="mr-2"> mdi-email </v-icon>
               Email
             </v-btn>
             <v-btn href="//github.com/coyotte508" target="blank">
-              <v-icon class="mr-2">
-                mdi-github
-              </v-icon>
+              <v-icon class="mr-2"> mdi-github </v-icon>
               Github
             </v-btn>
-            <v-btn href="https://stackoverflow.com/users/835629/coyotte508" target="blank">
-              <v-icon class="mr-2">
-                mdi-stack-overflow
-              </v-icon>
+            <v-btn
+              href="https://stackoverflow.com/users/835629/coyotte508"
+              target="blank"
+            >
+              <v-icon class="mr-2"> mdi-stack-overflow </v-icon>
               Stackoverflow
             </v-btn>
           </v-card-actions>
@@ -40,32 +49,39 @@
     </v-row>
     <v-row>
       <v-col cols="12" md="6">
-        <h1 class="text-center">
-          Projects
-        </h1>
+        <h1 class="text-center">Projects</h1>
 
-        <Experience v-for="project in projects" :key="project.id" :project="project" min-height="180" class="mt-6" />
+        <Experience
+          v-for="project in projects"
+          :key="project.id"
+          :project="project"
+          min-height="180"
+          class="mt-6"
+        />
       </v-col>
       <v-col cols="12" md="6">
-        <h1 class="text-center mb-3">
-          Experiences
-        </h1>
+        <h1 class="text-center mb-3">Experiences</h1>
 
-        <Experience v-for="project in works" :key="project.id" :project="project" class="mt-6" />
+        <Experience
+          v-for="project in works"
+          :key="project.id"
+          :project="project"
+          class="mt-6"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-import Experience from '../components/Experience.vue';
-import { projects, experiences } from '~/data/projects';
+import { Vue, Component } from "vue-property-decorator";
+import Experience from "../components/Experience.vue";
+import { projects, experiences } from "~/data/projects";
 
 @Component({
   components: {
-    Experience
-  }
+    Experience,
+  },
 })
 export default class Index extends Vue {
   projects = projects;
