ded6351a7a7f8ad9a295f19dbca512da2eed1bd0
coyotte508 <coyotte508@protonmail.com>
2026-08-02 11:53:43 +0000

    Add Svelte tech pill to Hugging Face

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -163,7 +163,7 @@ There are currently four boardgames on the platform: Gaia Project, Powergrid, Co
 export const experiences: Project[] = sortByEndYearDesc([
 	{
 		id: "huggingface",
-		technologies: ["node", "mongo", "agents", "kubernetes"],
+		technologies: ["node", "mongo", "svelte", "agents", "kubernetes"],
 		years: "2022 - now",
 		startYear: 2022,
 		title: "Hugging Face",
